f0f252e2d5cd9d0b6ce7bb7d5437aa5237b4e359
James Obogu <155890307+Jamon745@users.noreply.github.com>
2024-05-31 10:50:55 -0700

    commit

--- src/Components/SideBarButtons/CampaignButton.jsx
+++ src/Components/SideBarButtons/CampaignButton.jsx
@@ -12,7 +12,7 @@ const CampaignButton = () => {
   return (
     <div>
       <button onClick={handleClick}  className="hover:bg-buttonColor hover:text-white cursor-pointer text-left w-40 flex items-center py-2 mt-2 rounded-md">
-            <MdOutlineCampaign className="mr-4" /> Campaign
+            <MdOutlineCampaign className="mr-4" /> Campaign button
           </button>
 
     </div>
